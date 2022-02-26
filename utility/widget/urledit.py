9d6e46066be7a8c98f36f32c8685f24e5fa2e30e
chaosannals <chaosannals@outlook.com>
2022-02-26 09:40:58 +0800

    switch

--- utility/widget/urledit.py
+++ utility/widget/urledit.py
@@ -2,7 +2,7 @@ from typing import Sequence
 from urllib.parse import urlparse
 from loguru import logger
 from PySide6.QtCore import QStringListModel
-from PySide6.QtWidgets import QWidget, QCompleter
+from PySide6.QtWidgets import QWidget, QCompleter, QTableWidgetItem, QPushButton
 from .urledit_ui import Ui_UrlEdit
 
 
@@ -21,15 +21,12 @@ class UrlEdit(QWidget):
         self.ui.partitionNextButton.clicked.connect(self.onClickNextButton)
 
         self.ui.urlEdit.textEdited.connect(self.onEditedUrlEdit)
-        self.ui.protocolBox.currentIndexChanged.connect(self.onChangeProtocolBox)
-        self.ui.hostEdit.textEdited.connect(self.onEditedHostEdit)
-        self.ui.portBox.valueChanged.connect(self.onChangePortBox)
-        self.ui.pathEdit.textEdited.connect(self.onEditedPathEdit)
+        self.ui.queryAddButton.clicked.connect(self.onClickQueryAddButton)
         self.hostCompleter = QCompleter()
         self.ui.hostEdit.setCompleter(self.hostCompleter)
         self.pathCompleter = QCompleter()
         self.ui.pathEdit.setCompleter(self.pathCompleter)
-        self.resizeByMode()
+        self.onModeSwitch()
 
     @property
     def value(self) -> dict:
@@ -84,13 +81,24 @@ class UrlEdit(QWidget):
         self.ui.pathEdit.clear()
         self.ui.queryTable.clear()
 
-    def resizeByMode(self):
+    def onModeSwitch(self):
         n = self.ui.modeStack.currentIndex()
         if n == 1:
             h = self.ui.urlWidget.height()
+            self.updateUrl()
             self.setMaximumHeight(h)
         else:
             self.setMaximumHeight(16777215)
+
+    def addQueryRow(self):
+        rc = self.ui.queryTable.rowCount()
+        self.ui.queryTable.insertRow(rc)
+        db = QPushButton()
+        db.setText('删除')
+        db.setStyleSheet('QPushButton { color: #f00; background: #fff; margin: 5px; border: 1px solid #f00 }')
+        db.clicked.connect(lambda: self.onClickQueryTableItemDeleteButton(db))
+        self.ui.queryTable.setCellWidget(rc, 2, db)
+        return rc
 
     def updateUrl(self):
         '''
@@ -98,6 +106,24 @@ class UrlEdit(QWidget):
         '''
         port = '' if self.port is None else f':{self.port}'
         url = f'{self.protocol}://{self.host}{port}/{self.path}'
+
+        # 查询参数
+        query = []
+        for i in range(self.ui.queryTable.rowCount()):
+            ki = self.ui.queryTable.item(i, 0)
+            k = ki.text().strip()
+            if len(k) > 0:
+                vi = self.ui.queryTable.item(i, 1)
+                v = '' if vi is None else vi.text().strip()
+                query.append(f'{k}={v}')
+        if len(query) > 0:
+            url += '?' + '&'.join(query)
+
+        # 锚点 hash
+        h = self.ui.hashEdit.text().strip()
+        if len(h) > 0:
+            url = f'{url}#{h}'
+        
         self.ui.urlEdit.setText(url)
 
     def onClickClearButthon(self):
@@ -107,7 +133,7 @@ class UrlEdit(QWidget):
         i = self.ui.modeStack.currentIndex()
         c = self.ui.modeStack.count()
         self.ui.modeStack.setCurrentIndex((i + 1) % c)
-        self.resizeByMode()
+        self.onModeSwitch()
 
     def onEditedUrlEdit(self, text):
         '''
@@ -124,34 +150,33 @@ class UrlEdit(QWidget):
             self.ui.hostEdit.setText(pr.hostname)
             self.ui.portBox.setValue(pr.port)
             self.ui.pathEdit.setText(pr.path.strip('/'))
+            # 查询参数
+            self.ui.queryTable.clear()
+            self.ui.queryTable.setRowCount(0)
+            for q in pr.query.split('&'):
+                k, v = q.split('=', 2)
+                r = self.addQueryRow()
+                qki = QTableWidgetItem()
+                qki.setText(k)
+                self.ui.queryTable.setItem(r, 0, qki)
+                qvi = QTableWidgetItem()
+                qvi.setText(v)
+                self.ui.queryTable.setItem(r, 1, qvi)
+            self.ui.hashEdit.setText(pr.fragment)
         except ValueError as e:
             self.clearPartition()
 
-    def onChangeProtocolBox(self):
+    def onClickQueryAddButton(self):
         '''
         
         '''
 
-        self.updateUrl()
+        self.addQueryRow()
 
-    def onEditedHostEdit(self, text):
-        '''
-
-        '''
-
-        self.updateUrl()
-
-    def onChangePortBox(self, text):
-        '''
-
-        '''
-
-        self.updateUrl()
-
-
-    def onEditedPathEdit(self, text):
-        '''
-
-        '''
-
-        self.updateUrl()
+    def onClickQueryTableItemDeleteButton(self, db):
+        for i in range(self.ui.queryTable.rowCount()):
+            b = self.ui.queryTable.cellWidget(i, 2)
+            if b == db:
+                logger.info('删除第 {} 行请求参数', i + 1)
+                self.ui.queryTable.removeRow(i)
+                break
